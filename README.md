ef0eb10e37a6dd9e814c596228729efdb89dc9a7
stella <raven_uw@yahoo.com>
2020-06-28 14:45:10 +0800

    version 2020-06-28

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # pdbQuery
 根据蛋白信息一次性拉取全部PDB结构
+
+## How to deploy
+1. Firstly clone the project
+```{shell}
+$ git clone https://github.com/Stella3055/pdbQuery.git
+$ cd pdbQuery
+```
+2. \* Optional: activate virtual python environment/conda env if necessary
+```{shell}
+$ conda activate YOUR_ENV_NAME
+```
+
+3. Install the packages needed
+```{shell}
+$ python -m pip install -r ./requirements.txt
+# for help
+$ python ./pdbQuery.py --help
+# or
+$ python ./pdbQuery.py -h
+```
+
+4. Run
+```{shell}
+$ python ./pdbQuery.py P60484
+6 PDB structure(s) found: 2KYL,1D5R,4O1V,5BUG,5BZX,5BZZ
+Downloading PDB structure 2KYL (1/6)
+Downloading PDB structure 1D5R (2/6)
+Downloading PDB structure 4O1V (3/6)
+Downloading PDB structure 5BUG (4/6)
+Downloading PDB structure 5BZX (5/6)
+Downloading PDB structure 5BZZ (6/6)
+Extracting metadata from 2KYL (1/6)
+Extracting metadata from 1D5R (2/6)
+Extracting metadata from 4O1V (3/6)
+Extracting metadata from 5BUG (4/6)
+Extracting metadata from 5BZX (5/6)
+Extracting metadata from 5BZZ (6/6)
+
+# metadata
+$ cat ./PDB.metadata.txt
+2KYL    B                       SOLUTION STRUCTURE OF MAST2-PDZ COMPLEXED WITH THE C-TERMINUS OF PTEN
+1D5R    A       7-353   2.10    CRYSTAL STRUCTURE OF THE PTEN TUMOR SUPPRESSOR
+4O1V    B               2.00    SPOP PROMOTES TUMORIGENESIS BY ACTING AS A KEY REGULATORY HUB IN KIDNEY CANCER
+5BUG    A, B, C, D              2.40    CRYSTAL STRUCTURE OF HUMAN PHOSPHATASE PTEN OXIDIZED BY H2O2
+5BZX    A, B, C, D              2.50    CRYSTAL STRUCTURE OF HUMAN PHOSPHATASE PTEN TREATED WITH A BISPEROXOVANADIUM COMPLEX
+5BZZ    A, B, C, D              2.20    CRYSTAL STRUCTURE OF HUMAN PHOSPHATASE PTEN IN ITS REDUCED STATE
+```
+
+## Tips
+- Terminal proxy tools (such as [proxychains-ng](https://github.com/rofl0r/proxychains-ng)) may accelerate the fetch process
--- pdbQuery.py
+++ pdbQuery.py
@@ -4,6 +4,8 @@ import json
 import os
 import wget
 import re
+import getopt
+import sys
 
 def getPdbList(uniprotId):
     # https://search.rcsb.org/index.html#search-services
@@ -21,34 +23,58 @@ def batchQueryPdb(idList):
         os.mkdir("PDBs")
     else:
         print("Warning: directory PDBs has existed")
-    for idX in idList:
-        wget.download(DOWNLOAD_URL+idX+".pdb", out="PDBs")
+    for indX, idX in enumerate(idList):
+        print("Downloading PDB structure %s (%d/%d)" %(idX, indX+1, len(idList)))
+        wget.download(DOWNLOAD_URL+idX+".pdb", out="PDBs", bar=None)
     return
 
 def getMetaData(idList):
     # RegEx Patterns
     patternDesc = re.compile(r"TITLE.*")
     patternChain = re.compile(r"COMPND.*CHAIN: .*;")
-    patternChainDesc = re.compile(r"TITLE.*")
-    patternRange = re.compile(r"COMPND.*MOLECULE: [.\n]*;")
+    # patternChainDesc = re.compile(r"TITLE.*")
+    patternRange = re.compile(r"COMPND.*FRAGMENT: RESIDUES \d*-\d*;")
     patternRes = re.compile(r'REMARK.*RESOLUTION.\s*[\d"."]*\s*ANGSTROMS.')
-
     pdbList = list(map(lambda x: "PDBs/%s.pdb" %x.lower(), idList))
-    for pdbX in pdbList:
-        with open(pdbX, "r") as pdbFile:
-            pdbContent = pdbFile.read()
-            desc = (re.sub(r"\s*TITLE\s*\d*\s*", " ", "".join(patternDesc.findall(pdbContent))).strip())
-            print(desc)
-            chains = (re.sub(r"COMPND.*CHAIN:\s*", " ", "".join(patternChain.findall(pdbContent))).strip().replace(";", ""))
-            print(chains)
-            chainsDesc = (re.sub(r"COMPND.*CHAIN:\s*", " ", "".join(patternChain.findall(pdbContent))).strip().replace(";", ""))
-            print("_____________")
+    with open("PDB.metadata.txt", "w") as out_meta:
+        for indX, pdbX in enumerate(pdbList):
+            print("Extracting metadata from %s (%d/%d)" %(idList[indX], indX+1, len(pdbList)))
+            with open(pdbX, "r") as pdbFile:
+                pdbContent = pdbFile.read()
+                desc = (re.sub(r"\s*TITLE\s*\d*\s*", " ", "".join(patternDesc.findall(pdbContent))).strip())
+                chains = (re.sub(r"COMPND.*CHAIN:\s*", " ", "".join(patternChain.findall(pdbContent))).strip().replace(";", ""))
+                # chainsDesc = (re.sub(r"COMPND.*CHAIN:\s*", " ", "".join(patternChainDesc.findall(pdbContent))).strip().replace(";", ""))
+                ranges = (re.sub(r"COMPND.*FRAGMENT: RESIDUES\s*", " ", "".join(patternRange.findall(pdbContent))).strip().replace(";", ""))
+                res = (re.sub(r"REMARK.*RESOLUTION.", " ", "".join(patternRes.findall(pdbContent))).replace("ANGSTROMS.", "").strip())
 
+                out_meta.write("\t".join([
+                    idList[indX],
+                    chains,
+                    ranges,
+                    res,
+                    desc
+                ])+"\n")
+    return
 
+def main(uniprotId):
+    idList = getPdbList(uniprotId)
+    batchQueryPdb(idList)
+    getMetaData(idList)
+    return
 
 if __name__ == "__main__":
-    TARGET = "P60484"
-    # idList = getPdbList(TARGET)
-    idList = ['2KYL', '1D5R', '4O1V', '5BUG', '5BZX', '5BZZ']
-    getMetaData(idList)
-    # batchQueryPdb(idList)
+    if len(sys.argv) == 1 or sys.argv[1] in ["-h", "--help"]:
+        print('''
+---------------------------
+pdbQuery: fetch PDB structures related to a certain UniprotID or Keyword and extract their metadata.
+Usage:
+# Deploy environment
+[PYTHON_ENV] python -m pip install -r ./requirements.txt
+# Download and analyze
+[PYTHON_ENV] python ./pdbQuery.py [UNIPROT_ID]
+# e.g.
+python ./pdbQuery.py P60484
+---------------------------
+        ''')
+    else:
+        main(str(sys.argv[1]))
